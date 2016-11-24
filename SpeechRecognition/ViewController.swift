55d19d256e61704c49e778c18de962a94590f455
Stephen Anthony <steve@darjeelingapps.com>
2016-11-24 16:23:25 +0000

    Initial Commit

---
+++ SpeechRecognition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpeechRecognition
+//
+//  Created by Stephen Anthony on 24/11/2016.
+//  Copyright © 2016 Darjeeling Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
